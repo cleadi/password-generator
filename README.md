d11631a8b0f1b78ed01ead939390f603ab484876
Dillon Cleaver <cleadi@fastmail.com>
2021-09-21 17:07:41 -0500

    Fixed some bugs, added some extra code and refactored some existing code.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # password-generator
 Boot Camp HW #3
+
+## Project Description
+
+- This application generates a new password that is between 8 and 128 characters in length, composed of a random mix of numeric, uppercase, lowercase, and/or special characters. The user will get prompted to try again if they fail to enter a numeric value between 8 and 128.
+- The user is prompted to start the password generation process when they click on the "Generate Password" button displayed on the middle of the webpage.
+- The user will then select "Cancel" -- which is no -- or "OK" -- which is yes -- for whether or not the password should include numeric, uppercase, lowercase, and/or special characters.
+- Once the user has selected yes or no to all the prompts, a password will be displayed beneath the title "Generate a password" on the same webpage.
+
+## Website link
+https://cleadi.github.io/password-generator/
--- script.js
+++ script.js
@@ -1,90 +1,60 @@
 var generateBtn = document.querySelector("#generate"); // DON'T TOUCH
 
-const allCharacters = [specialCharacters, numbersList, uppercaseList, lowercaseList];
-
-// Use .charAt to parse the strings for characters to build the password??
-var specialCharacters = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+var specialCharactersList = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 var numbersList = "1234567890";
 var uppercaseList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowercaseList = "abcdefghijklmnopqrstuvwxyz";
 
 var numbersAllowed;
+var chosenCharacters;
+var specialCharsAllowed;
 var lowercaseAllowed;
 var uppercaseAllowed;
-var specialCharsAllowed;
-
 var numberOfCharacters;
-var allowedCharacters;
+var finalPassword;
 
-var userInput = "";
-var finalPassword = "";
+function generatePassword() {
+  finalPassword;
+  chosenCharacters = "";
+  numberOfCharacters = parseInt(window.prompt("How many characters would you like the password to be? Can be between 8-128 characters?"));
+  numbersAllowed = window.confirm("Would you like to use numbers?");
+  lowercaseAllowed = window.confirm("Would you like to use lowercase letters?");
+  uppercaseAllowed = window.confirm("Would you like to use uppercase characters?");
+  specialCharsAllowed = window.confirm("Would you like to use special characters?");
 
-function askHowManyCharacters() {
-  numberOfCharacters = window.prompt("How many characters would you like the password to be? Can be between 8-128 characters?");
-  if (userInput >= 8 && userInput <= 128) {
-    numberOfCharacters === true;
+  if (numberOfCharacters >= 8 && numberOfCharacters <= 128) {
+    if (numbersAllowed === true || specialCharsAllowed === true || lowercaseAllowed === true || uppercaseAllowed === true) {
+      getFinalCharactersAllowed();
+      finalPassword = buildPassword();
+    }
   } else {
     alert("Try again! Number of characters needs to be between 8 & 128."); // This doesn't work as it should
   }
-}
 
-function askNumbersAllowed() {
-  numbersAllowed = window.confirm("Would you like to use numbers?");
-  return numbersAllowed;
-}
-
-function askLowercaseAllowed() {
-  lowercaseAllowed = window.confirm("Would you like to use lowercase letters?");
-  return lowercaseAllowed;
-}
-
-function askUppercaseAllowed() {
-  uppercaseAllowed = window.confirm("Would you like to use uppercase characters?");
-  return uppercaseAllowed;
-}
-
-function askSpecialCharsAllowed() {
-  specialCharsAllowed = window.confirm("Would you like to use special characters?");
-  return specialCharsAllowed;
-}
-
-function genRandomChars() {
-  for (var i =0; i < numberOfCharacters; i++) {
-    numberOfCharacters += allowedCharacters [Math.floor(Math.random() * allowedCharacters.length)];
-  }
-}
-
-function generatePassword() {
-  numberOfCharacters = askHowManyCharacters();
-  numbersAllowed = askNumbersAllowed();
-  lowercaseAllowed = askLowercaseAllowed();
-  uppercaseAllowed = askUppercaseAllowed();
-  specialCharsAllowed = askSpecialCharsAllowed();
-  validation(numberOfCharacters, numbersAllowed, specialCharsAllowed, lowercaseAllowed, uppercaseAllowed);
   return finalPassword;
 }
 
-function validation(numberOfCharacters, numbersAllowed, specialCharsAllowed, lowercaseAllowed, uppercaseAllowed) {
-  window.confirm("Are these correct?");
-    if (numberOfCharacters === true || specialCharsAllowed === true || lowercaseAllowed === true || uppercaseAllowed === true) {
-    buildPassword();
+function buildPassword() {
+  var pw = "";
+  for (var i = 0; i < numberOfCharacters; i++) {
+    pw += chosenCharacters.charAt(Math.floor(Math.random() * chosenCharacters.length));
   }
+  return pw;
 }
 
-function buildPassword() {
+function getFinalCharactersAllowed() {
   if (numbersAllowed === true) {
-    allCharacters = allCharacters.concat(numbersList);
+    chosenCharacters = chosenCharacters.concat(numbersList);
   }
   if (specialCharsAllowed === true) {
-    allCharacters = allCharacters.concat(specialCharsAllowed);
+    chosenCharacters = chosenCharacters.concat(specialCharsAllowed);
   }
   if (lowercaseAllowed === true) {
-    allCharacters = allCharacters.concat(lowercaseAllowed);
+    chosenCharacters = chosenCharacters.concat(lowercaseAllowed);
   }
   if (uppercaseAllowed === true) {
-    allCharacters = allCharacters.concat(uppercaseAllowed);
+    chosenCharacters = chosenCharacters.concat(uppercaseAllowed);
   }
-  genRandomChars(); // Or generatePassword??
 }
 
 // DON'T TOUCH
@@ -92,9 +62,7 @@ function buildPassword() {
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // DON'T TOUCH 
