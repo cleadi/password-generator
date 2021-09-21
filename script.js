6a905e5e801bc1bb126c52ced419d1180d8c46b8
Dillon Cleaver <cleadi@fastmail.com>
2021-09-20 22:25:51 -0500

    Make some edits to the script.js file. Still needs some work.

--- script.js
+++ script.js
@@ -1,85 +1,95 @@
-var generateBtn = document.querySelector("#generate"); // DON'T TOUCH // 
+var generateBtn = document.querySelector("#generate"); // DON'T TOUCH
 
-var specialCharacters = ("!#$%&'()*+,-./:;<=>?@[]^_`{|}~");
-var numbersList = ("1234567890");
-var uppercaseList = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-var lowercaseList = ("abcdefghijklmnopqrstuvwxyz");
+const allCharacters = [specialCharacters, numbersList, uppercaseList, lowercaseList];
+
+// Use .charAt to parse the strings for characters to build the password??
+var specialCharacters = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+var numbersList = "1234567890";
+var uppercaseList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowercaseList = "abcdefghijklmnopqrstuvwxyz";
+
+var numbersAllowed;
+var lowercaseAllowed;
+var uppercaseAllowed;
+var specialCharsAllowed;
+
+var numberOfCharacters;
+var allowedCharacters;
+
+var userInput = "";
+var finalPassword = "";
 
 function askHowManyCharacters() {
-  var numberOfCharacters = window.prompt("How many characters would you like the password to be? Can be between 8-128 characters?");
-  console.log(numberOfCharacters);
-  return numberOfCharacters;
+  numberOfCharacters = window.prompt("How many characters would you like the password to be? Can be between 8-128 characters?");
+  if (userInput >= 8 && userInput <= 128) {
+    numberOfCharacters === true;
+  } else {
+    alert("Try again! Number of characters needs to be between 8 & 128."); // This doesn't work as it should
+  }
 }
 
 function askNumbersAllowed() {
-  var numbersAllowed = window.confirm("Would you like to use numbers?");
-  console.log(numbersAllowed);
+  numbersAllowed = window.confirm("Would you like to use numbers?");
   return numbersAllowed;
 }
 
 function askLowercaseAllowed() {
-  var lowercaseAllowed = window.confirm("Would you like to use lowercase letters?");
-  console.log(lowercaseAllowed);
+  lowercaseAllowed = window.confirm("Would you like to use lowercase letters?");
   return lowercaseAllowed;
 }
 
 function askUppercaseAllowed() {
-  var uppercaseAllowed = window.confirm("Would you like to use uppercase characters?");
-  console.log(uppercaseAllowed);
+  uppercaseAllowed = window.confirm("Would you like to use uppercase characters?");
   return uppercaseAllowed;
 }
 
 function askSpecialCharsAllowed() {
-  var specialCharsAllowed = window.confirm("Would you like to use special characters?");
-  console.log(specialCharsAllowed);
+  specialCharsAllowed = window.confirm("Would you like to use special characters?");
   return specialCharsAllowed;
 }
 
-// Seems like this code will somehow help create a password that is x number of characters long
-// for (var i = 0; i < 5; i++) {
-//   console.log("This is the current value of i: " + i + ".");
-// }
+function genRandomChars() {
+  for (var i =0; i < numberOfCharacters; i++) {
+    numberOfCharacters += allowedCharacters [Math.floor(Math.random() * allowedCharacters.length)];
+  }
+}
 
-function generatePassword(){
-  var finalPassword = "";
-  var numberOfCharacters = askHowManyCharacters();
-  var numbersAllowed = askNumbersAllowed();
-  var lowercaseAllowed = askLowercaseAllowed();
-  var uppercaseAllowed = askUppercaseAllowed();
-  var specialCharsAllowed = askSpecialCharsAllowed();
+function generatePassword() {
+  numberOfCharacters = askHowManyCharacters();
+  numbersAllowed = askNumbersAllowed();
+  lowercaseAllowed = askLowercaseAllowed();
+  uppercaseAllowed = askUppercaseAllowed();
+  specialCharsAllowed = askSpecialCharsAllowed();
   validation(numberOfCharacters, numbersAllowed, specialCharsAllowed, lowercaseAllowed, uppercaseAllowed);
   return finalPassword;
 }
 
 function validation(numberOfCharacters, numbersAllowed, specialCharsAllowed, lowercaseAllowed, uppercaseAllowed) {
   window.confirm("Are these correct?");
-  if (numberOfCharacters >= 8 || numberOfCharacters <= 128) {
     if (numberOfCharacters === true || specialCharsAllowed === true || lowercaseAllowed === true || uppercaseAllowed === true) {
-    }
     buildPassword();
   }
 }
 
-// The job of this function is to pick one random element out of ANY array we give to it
-// function getRandomValue(arr){
-//   var result = arr[Math.floor(Math.random() * arr.length)];
-//   return result;
-// }
-
-// if (this) {then do this} else { do this }
-
-// JavaScipt string methods??
-function buildPassword(numbersAllowed, specialCharsAllowed, lowercaseAllowed, uppercaseAllowed) {
-  var allCharacters = ""
-  if (numbersAllowed === true) { allCharacters = allCharacters.concat(numbersList) } // What is concat??
-  if (specialCharsAllowed === true) { allCharacters = allCharacters.concat(specialCharsAllowed) }
-  if (lowercaseAllowed === true) { allCharacters = allCharacters.concat(lowercaseAllowed) }
-  if (uppercaseAllowed === true) { allCharacters = allCharacters.concat(uppercaseAllowed) }
-  console.log(allCharacters);
+function buildPassword() {
+  if (numbersAllowed === true) {
+    allCharacters = allCharacters.concat(numbersList);
+  }
+  if (specialCharsAllowed === true) {
+    allCharacters = allCharacters.concat(specialCharsAllowed);
+  }
+  if (lowercaseAllowed === true) {
+    allCharacters = allCharacters.concat(lowercaseAllowed);
+  }
+  if (uppercaseAllowed === true) {
+    allCharacters = allCharacters.concat(uppercaseAllowed);
+  }
+  genRandomChars(); // Or generatePassword??
 }
 
+// DON'T TOUCH
 // Write password to the #password input
-function writePassword() { // DON'T TOUCH
+function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -87,5 +97,6 @@ function writePassword() { // DON'T TOUCH
 
 }
 
+// DON'T TOUCH 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword); // DON'T TOUCH 
+generateBtn.addEventListener("click", writePassword);
